7520dfea0d5f923ddeec17329e72a5c75bef80e8
aqolys <sharelove.main@gmail.com>
2024-02-19 21:56:41 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Home from '@/components/Home.vue'
+
+export default {
+  components: {
+    Home,
+  },
+}
+</script>
+
+<template>
+  <!-- <header class="p-4">
+    <span class="text-4xl font-bold tracking-tight">Lunsjliste</span>
+  </header> -->
+  <main>
+    <Home />
+  </main>
+</template>
+
+<style scoped></style>
